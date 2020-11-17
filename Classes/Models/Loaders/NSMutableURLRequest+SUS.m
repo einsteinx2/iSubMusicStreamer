91a1ba7a4c8847c76aef19dfff4c283f41e5ab88
Ben Baron <ben@einsteinx2.com>
2020-11-17 15:54:28 -0500

    Switched to GET requests as POST didn't work sometimes

--- Classes/Models/Loaders/NSMutableURLRequest+SUS.m
+++ Classes/Models/Loaders/NSMutableURLRequest+SUS.m
@@ -20,9 +20,7 @@ static NSArray *ver1_6_0 = nil;
 static NSArray *ver1_8_0 = nil;
 static NSSet *setOfVersions = nil;
 
-+ (void)initialize
-{
-//DLog(@"NSMutableURLRequest initialize called");
++ (void)initialize {
     ver1_0_0 = @[@"ping", @"getLicense", @"getMusicFolders", @"getNowPlaying", @"getIndexes", @"getMusicDirectory", @"search", @"getPlaylists", @"getPlaylist", @"download", @"stream", @"getCoverArt", @"1.0.0"];
     ver1_2_0 = @[@"createPlaylist", @"deletePlaylist", @"getChatMessages", @"addChatMessage", @"getAlbumList", @"getRandomSongs", @"getLyrics", @"jukeboxControl", @"1.2.0"];
     ver1_3_0 = @[@"getUser", @"deleteUser", @"1.3.0"];
@@ -33,8 +31,7 @@ static NSSet *setOfVersions = nil;
     setOfVersions = [[NSSet alloc] initWithObjects:ver1_0_0, ver1_2_0, ver1_3_0, ver1_4_0, ver1_5_0, ver1_6_0, ver1_8_0, nil];
 }
 
-+ (NSMutableURLRequest *)requestWithSUSAction:(NSString *)action urlString:(NSString *)url username:(NSString *)user password:(NSString *)pass parameters:(NSDictionary *)parameters byteOffset:(NSUInteger)offset
-{
++ (NSMutableURLRequest *)requestWithSUSAction:(NSString *)action urlString:(NSString *)url username:(NSString *)user password:(NSString *)pass parameters:(NSDictionary *)parameters byteOffset:(NSUInteger)offset {
     NSMutableString *urlString = [NSMutableString stringWithFormat:@"%@/rest/%@.view", url, action];
     if ([action isEqualToString:@"hls"])
         urlString = [NSMutableString stringWithFormat:@"%@/rest/%@.m3u8", url, action];
@@ -43,10 +40,8 @@ static NSSet *setOfVersions = nil;
 	NSString *version = nil;
 	
 	// Set the API version for this call by checking the arrays
-	for (NSArray *versionArray in setOfVersions)
-	{
-		if ([versionArray containsObject:action])
-		{
+	for (NSArray *versionArray in setOfVersions) {
+		if ([versionArray containsObject:action]) {
 			version = [versionArray lastObject];
 			break;
 		}
@@ -56,37 +51,25 @@ static NSSet *setOfVersions = nil;
 	// Setup the POST parameters
 	//NSMutableString *postString = [NSMutableString stringWithFormat:@"v=%@&c=iSub", version];
 	NSMutableString *postString = [NSMutableString stringWithFormat:@"v=%@&c=iSub&u=%@&p=%@", version, username, password];
-	if (parameters != nil)
-	{
-		for (NSString *key in [parameters allKeys])
-		{
-			if ((NSNull *)[parameters objectForKey:key] == [NSNull null])
-			{
+	if (parameters != nil) {
+		for (NSString *key in [parameters allKeys]) {
+			if ((NSNull *)[parameters objectForKey:key] == [NSNull null]) {
 				//if ([NSThread respondsToSelector:@selector(callStackSymbols)])
                     //DLog(@"Received a null parameter for key: %@ for action: %@  stack trace:\n%@", key, action, [NSThread callStackSymbols]);
-			}
-			else
-			{
+			} else {
 				id value = [parameters objectForKey:key];
-				if ([value isKindOfClass:[NSArray class]])
-				{
+				if ([value isKindOfClass:[NSArray class]]) {
 					// handle multiple values for key
-					for (id subValue in (NSArray*)value)
-					{
-						if ([subValue isKindOfClass:[NSString class]])
-						{
+					for (id subValue in (NSArray*)value) {
+						if ([subValue isKindOfClass:[NSString class]]) {
 							// handle single value for key
 							[postString appendFormat:@"&%@=%@", [key URLEncodeString], [(NSString*)subValue URLEncodeString]];
-						}
-                        else if ([subValue isKindOfClass:[NSNumber class]])
-                        {
+						} else if ([subValue isKindOfClass:[NSNumber class]]) {
                             // Convert numbers to strings
 							[postString appendFormat:@"&%@=%@", [key URLEncodeString], [[(NSNumber *)subValue stringValue] URLEncodeString]];
                         }
 					}
-				}
-				else if ([value isKindOfClass:[NSString class]])
-				{
+				} else if ([value isKindOfClass:[NSString class]]) {
 					// handle single value for key
 					[postString appendFormat:@"&%@=%@", [key URLEncodeString], [(NSString*)value URLEncodeString]];
 				}
@@ -97,33 +80,18 @@ static NSSet *setOfVersions = nil;
 	
 	// Handle special case when loading playlists
 	NSTimeInterval loadingTimeout = ISMSLoadingTimeout;
-	if ([action isEqualToString:@"getPlaylist"])
-	{
+	if ([action isEqualToString:@"getPlaylist"]) {
 		loadingTimeout = 3600.0; // Timeout set to 60 mins to prevent timeout errors
-	}
-	else if ([action isEqualToString:@"ping"])
-	{
+	} else if ([action isEqualToString:@"ping"]) {
 		loadingTimeout = ISMSServerCheckTimeout;
 	}
 	
-	if ([url isEqualToString:@"https://one.ubuntu.com/music"])
-	{
-		// This is Ubuntu One, send as GET request
-		[urlString appendFormat:@"?%@", postString];
-	}
-	
 	// Create the request
-	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString] 
-									  cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData 
-								  timeoutInterval:loadingTimeout];
-	
-    [request setHTTPMethod:@"POST"];
-    [request setHTTPBody:[postString dataUsingEncoding:NSUTF8StringEncoding]];
-    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-	
+    [urlString appendFormat:@"?%@", postString];
+	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString] cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:loadingTimeout];
+    
 	// Set the HTTP Basic Auth
-	if (settingsS.isBasicAuthEnabled)
-	{
+    if (settingsS.isBasicAuthEnabled) {
 		//DLog(@"using basic auth!");
 		NSString *authStr = [NSString stringWithFormat:@"%@:%@", username, password];
 		NSData *authData = [authStr dataUsingEncoding:NSASCIIStringEncoding];
@@ -131,8 +99,7 @@ static NSSet *setOfVersions = nil;
 		[request setValue:authValue forHTTPHeaderField:@"Authorization"];
 	}
 	
-	if (offset > 0)
-	{
+	if (offset > 0) {
 		NSString *rangeString = [NSString stringWithFormat:@"bytes=%ld-", (long)offset];
 		[request setValue:rangeString forHTTPHeaderField:@"Range"];
 	}
@@ -145,18 +112,15 @@ static NSSet *setOfVersions = nil;
     return request;
 }
 
-+ (NSMutableURLRequest *)requestWithSUSAction:(NSString *)action urlString:(NSString *)url username:(NSString *)user password:(NSString *)pass parameters:(NSDictionary *)parameters
-{
++ (NSMutableURLRequest *)requestWithSUSAction:(NSString *)action urlString:(NSString *)url username:(NSString *)user password:(NSString *)pass parameters:(NSDictionary *)parameters {
 	return [NSMutableURLRequest requestWithSUSAction:action urlString:url username:user password:pass parameters:nil byteOffset:0];
 }
 
-+ (NSMutableURLRequest *)requestWithSUSAction:(NSString *)action parameters:(NSDictionary *)parameters byteOffset:(NSUInteger)offset
-{
++ (NSMutableURLRequest *)requestWithSUSAction:(NSString *)action parameters:(NSDictionary *)parameters byteOffset:(NSUInteger)offset {
     return [NSMutableURLRequest requestWithSUSAction:action urlString:settingsS.urlString username:settingsS.username password:settingsS.password parameters:parameters byteOffset:offset];
 }
 
-+ (NSMutableURLRequest *)requestWithSUSAction:(NSString *)action parameters:(NSDictionary *)parameters
-{
++ (NSMutableURLRequest *)requestWithSUSAction:(NSString *)action parameters:(NSDictionary *)parameters {
 	return [NSMutableURLRequest requestWithSUSAction:action parameters:parameters byteOffset:0];
 }
 
