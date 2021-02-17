75b636184f2a666796e414e894198317101ac6a0
Ben Baron <ben@einsteinx2.com>
2021-02-17 12:32:42 -0800

    Added more XML string cleaning extensions

--- Classes/Extensions/Foundation/String+Clean.swift
+++ Classes/Extensions/Foundation/String+Clean.swift
@@ -8,6 +8,30 @@
 
 import Foundation
 
+// This is the format that my server seems to reply with
+private let iso8601FormatterWithMilliseconds: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.calendar = Calendar(identifier: .iso8601)
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    return dateFormatter
+}()
+
+// This is the format shown in the documentation
+private let iso8601FormatterWithoutTimezone: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.calendar = Calendar(identifier: .iso8601)
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+    return dateFormatter
+}()
+
+private func formatDate(dateString: String) -> Date? {
+    iso8601FormatterWithMilliseconds.date(from: dateString) ?? iso8601FormatterWithoutTimezone.date(from: dateString)
+}
+
 private extension String {
     // TODO: Investigate what problem I was solving using this process. I know I did it for some reason many years ago, but now I have no idea ¯\_(ツ)_/¯
     // NOTE: Presumably Subsonic was sending back some characters using HTML encoding for some reason...need to confirm that.
@@ -46,6 +70,17 @@ extension Optional where Wrapped == String {
         }
     }
     
+    var doubleXML: Double {
+        doubleXMLOptional ?? 0
+    }
+    var doubleXMLOptional: Double? {
+        if let self = self {
+            return Double(self.clean())
+        } else {
+            return nil
+        }
+    }
+    
     var boolXML: Bool {
         boolXMLOptional ?? false
     }
@@ -57,5 +92,14 @@ extension Optional where Wrapped == String {
         }
     }
     
-    
+    var dateXML: Date {
+        dateXMLOptional ?? .distantPast
+    }
+    var dateXMLOptional: Date? {
+        if let self = self {
+            return formatDate(dateString: self)
+        } else {
+            return nil
+        }
+    }
 }
