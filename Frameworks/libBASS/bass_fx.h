6811d5141336897975ecd439c8452873fcd2c2d6
Ben Baron <ben@einsteinx2.com>
2024-02-23 20:42:02 -0500

    Fix all remaining Xcode 15 warnings

--- Frameworks/libBASS/bass_fx.h
+++ Frameworks/libBASS/bass_fx.h
@@ -40,7 +40,7 @@ typedef int BOOL32;
 #define BASS_FX_FREESOURCE			0x10000	// Free the source handle as well?
 
 // BASS_FX Version
-DWORD BASS_FXDEF(BASS_FX_GetVersion)();
+DWORD BASS_FXDEF(BASS_FX_GetVersion)(void);
 
 /*===========================================================================
 	DSP (Digital Signal Processing)
