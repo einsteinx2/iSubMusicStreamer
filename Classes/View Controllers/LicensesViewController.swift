98c4c0454db4c965bfce3a95d77be714799f0995
Matt Neuburg <matt@tidbits.com>
2024-10-27 10:04:11 -0700

    fix call-super and related howlers (#91)

--- Classes/View Controllers/LicensesViewController.swift
+++ Classes/View Controllers/LicensesViewController.swift
@@ -14,6 +14,7 @@ import SnapKit
     let webView = WKWebView()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
         
         webView.allowsBackForwardNavigationGestures = true
